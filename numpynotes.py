b110e5aca0a7f37b7e15b1e70a3dd180faf3bd62
Kayla Brock <kayla.marie.brock@gmail.com>
2022-04-18 11:25:41 -0500

    update

---
+++ numpynotes.py
@@ -0,0 +1,11 @@
+'''What is Numpy? 
+Multidimensional array library 
+Numpy is very fast therefore it is preferred over lists 
+Contiguous Memory 
+Fixed types
+SIMD Vector Processing 
+Effective Cache Utilization 
+'''
+
+import numpy as np
+
